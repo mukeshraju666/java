81261b1a3066fa697139c3c35576e92fd0872088
mukeshraju666 <mukeshraju666@gmail.com>
2025-07-11 00:43:36 +0530

    Switch Statement

---
+++ corejava/Switchcondition.java
@@ -0,0 +1,33 @@
+package corejava;
+class Switchcondition{
+    public static void main(String[]args){
+        int days = 1;
+        switch(days){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("TUESDAY");
+                break;
+            case 3:
+                System.out.println("WEDNESDAY");
+                break;
+            case 4:
+                System.out.println("THURSDAY");
+                break;    
+            case 5:
+                System.out.println("FRIDAY");
+                break;
+            case 6:
+                System.out.println("SATURDAY");
+                break; 
+            case 7:
+                System.out.println("SUNDAY");
+                break;   
+
+            default:
+                break;
+
+        }
+    }
+}
