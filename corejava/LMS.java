1d1ec4a31d5e67db005fa02487046dca901591a1
mukeshraju666 <mukeshraju666@gmail.com>
2025-07-10 23:57:59 +0530

    Lms Basics

---
+++ corejava/LMS.java
@@ -0,0 +1,27 @@
+package corejava;
+class LMS{
+    public static void main(String []args){
+        String studentName = "Mukesh";
+        int studentId = 101;
+        char studentGender = 'M';
+        boolean isTrainer = true;
+
+        // courses 
+        String courseName = "Java";
+        int courseID = 111;
+        float courseDuration = 4;
+        int totalCredits = 5;
+
+        System.out.println("Student Name :" + studentName);
+        System.out.println("Student ID :" + studentId);
+        System.out.println("Student Gender :" + studentGender);
+        System.out.println("Trainer Available :" + isTrainer);
+        System.out.println("Student CourseName :" + courseName);
+        System.out.println("Student CourseID :" + courseID);
+        System.out.println("Student CourseDuration :" + courseDuration);
+        System.out.println("Student Total Credits :" + totalCredits);
+
+        
+
+    }
+}
