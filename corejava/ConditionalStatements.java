0232b8677f272392991518bd839ad1eed1c19a42
mukeshraju666 <mukeshraju666@gmail.com>
2025-07-11 00:33:23 +0530

    Grades and Numberts in Range

--- corejava/ConditionalStatements.java
+++ corejava/ConditionalStatements.java
@@ -23,5 +23,29 @@ class ConditionalStatements{
         }else {
             System.out.println("Enter Numbers(AGE) only");
         }
+
+        // Check the given number is in Range or Not;
+        // Range is 20 to 30;
+
+        int numRange = 5;
+        if(numRange >=20 && numRange<=30){
+            System.out.println("Given number is in Range");
+        }else{
+            System.out.println("Not in Range");
+        }
+
+
+        // Check the Grade Systems
+
+        int score = 1;
+        if(score >= 90){
+            System.out.println("GRADE A");
+        }else if (score >=75){
+            System.out.println("GRADE B");
+        }else if (score >=50){
+            System.out.println("GRADE C");
+        }else{
+            System.out.println("FAIL");
+        }
     }
 }
